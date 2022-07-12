1cf75fb07163b457fdce8f932715604519c2ddd7
g0rillaa <50138575+g0rillaa@users.noreply.github.com>
2022-07-12 14:26:25 +1000

    photos

---
+++ photos/scripts/album.js
@@ -0,0 +1,33 @@
+function addAlbumTest(albumData){
+    var container = $('#albumsContainer')
+    var album = $('<div>', {
+        class: 'albumContainer',
+    }).appendTo(container);
+    var header = $('<div>', {
+        class: 'albumHeaderContainer',
+    }).appendTo(album);
+    var title = $('<h2>', {
+        class: 'albumTitle',
+        html: albumData.title,
+    }).appendTo(header);
+    title.click(() => {
+        window.open(`https://gorillaa.net/photos/album/${albumData.path}`)
+    })
+    $('<h2>', {
+        class: 'albumDate',
+        html: albumData.date,
+    }).appendTo(header);
+    var photos = $('<div>', {
+        class: 'albumPhotosContainer',
+    }).appendTo(album);
+    albumData.photoSrcs.forEach(photo => {
+        var img = $('<img>', {
+            src: `./photos/${photo}-min.jpg`,
+            class: 'showcaseImg'
+        }).appendTo(photos);
+        img.click(() => {
+            window.open(`https://gorillaa.net/photos/album/${albumData.path}`)
+        })
+    })
+}
+
---
+++ photos/scripts/main.js
@@ -0,0 +1,24 @@
+var album1 = {
+    title: 'Celestial JDM Mini Meet',
+    date: '14 June 2022',
+    photoSrcs: ['5','6','7','8','5','6','7','8'],
+    path: 'celestial-jdm-mini-meet',
+}
+var album2 = {
+    title: 'epic meet',
+    date: '1444 Jun3e 2022',
+    photoSrcs: ['5','6','7','8','5','6','7','8'],
+    path: '1',
+}
+var album3 = {
+    title: 'bruhhh meet',
+    date: '14 aaaa 1693',
+    photoSrcs: ['5','6','7','8','5','6','7','8'],
+    path: '2',
+}
+
+$( document ).ready(function() {
+    addAlbumTest(album1)
+    addAlbumTest(album2)
+    addAlbumTest(album3)
+ });
